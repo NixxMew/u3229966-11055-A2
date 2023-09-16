e14ba9857da053c8a7bd75164568502fa1b637fd
Tydah Ngep <145172268+NixxMew@users.noreply.github.com>
2023-09-16 11:47:27 +1000

    Update week6.js

--- week6.js
+++ week6.js
@@ -1,3 +1,33 @@
-/*
-    Paste the code for your week 3 exercise below.
-*/
+// Set colour values
+let colour = 0
+let colourInc = 1
+
+//Set up canvas size, colour, colour modes
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+  colorMode(HSB, 255);
+}
+
+function draw() {
+  //Removes black border around
+  strokeWeight(0);
+
+  if (mouseIsPressed) {
+    
+    //Colour increments
+    colour = colour + colourInc;
+    if (colour > 255){
+      colour = 255;
+      colourInc *= -1;
+    }
+    else if(colour < 0){
+      colour = 0;
+      colourInc *= -1;
+    }
+    
+    // Change colour and draw ellipses at mouse
+    fill(colour, 255, 255);
+    ellipse(mouseX, mouseY, random(10,50), random(10,50));
+  }
+}
