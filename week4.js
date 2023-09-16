fdf6e961abf3ed79fad667ac423b61ad31d468e3
Tydah Ngep <145172268+NixxMew@users.noreply.github.com>
2023-09-16 11:20:42 +1000

    Update week4.js

--- week4.js
+++ week4.js
@@ -1,3 +1,18 @@
-/*
-    Paste the code for your week 4 exercise below.
-*/
+// Canvas setup
+function setup() {
+  createCanvas(500, 500);
+}
+
+// Drawing the logo
+function draw() {
+  background(50);
+  strokeWeight(0)
+  square( 100, 100, 300 );
+  fill('black')
+  square(150, 150, 75)
+  square(275, 150, 75)
+  rect(225, 225, 50, 75)
+  rect(175, 260, 50, 90)
+  rect(275, 260, 50, 90)
+  fill('green')
+}
